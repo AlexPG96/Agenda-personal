715abc07c8f4ff501d9660548b2f8c55333acfca
Alejandro <ap96.gonzalez@gmail.com>
2020-05-06 15:04:46 +0200

    funcion de filtrado y boton eliminar corregido

--- js/segundaprueba.js
+++ js/segundaprueba.js
@@ -4,17 +4,17 @@ var listaTareas = new Array();
 
 listaTareas = [
     {
-        'idTarea': 1,
+        'idTarea': 0,
         'tarea': 'Estudiar JavaScript',
-        'prioridad': 'urgente'
-    },
-    {
-        'idTarea': 2,
-        'tarea': 'Hacer la comida',
         'prioridad': 'diaria'
     },
     {
-        'idTarea': 3,
+        'idTarea': 1,
+        'tarea': 'Hacer la comida',
+        'prioridad': 'mensual'
+    },
+    {
+        'idTarea': 2,
         'tarea': 'Pegarme un tiro',
         'prioridad': 'urgente'
     }
@@ -22,13 +22,13 @@ listaTareas = [
 ];
 
 
-var idTarea = 3;
+var idTarea = 2;
 
 var seccionTareas = document.querySelector('#tareas');
 
-function pintarUnaTarea(pObjetoTarea){
-    seccionTareas.innerHTML += `<article id="${pObjetoTarea.idTarea}" class="${pObjetoTarea.prioridad}">
-    <div class="eliminar">Eliminar</div>
+function pintarUnaTarea(pObjetoTarea) {
+    seccionTareas.innerHTML += `<article class="${pObjetoTarea.prioridad}">
+    <div id="${pObjetoTarea.idTarea}" class="eliminar">Eliminar</div>
     <h3>${pObjetoTarea.tarea}</h3>
     </article>`
 }
@@ -42,9 +42,13 @@ function pintarTareas(pListaTareas) {
         pintarUnaTarea(tarea)
 
     }
+    var listaBotones = document.querySelectorAll('.eliminar');
 
-    botonEliminar(pIdTarea)
-    
+    for (eliminar of listaBotones) {
+        eliminar.addEventListener('click', botonEliminar);
+    }
+
+
 }
 pintarTareas(listaTareas);
 
@@ -57,7 +61,7 @@ function añadirTarea(event) {
 
     let tarea = document.querySelector('#nuevaTarea').value;
     let prioridad = document.querySelector('#agregarTarea').value;
-  
+
 
     if (tarea != "" && prioridad != "") {
         //si es distinto de vacío, le digo que me guarde la tarea en otra funcion y que me deje los campos vacíos.
@@ -79,11 +83,11 @@ function guardarTarea(pTarea, pPrioridad) {
     idTarea++;
 
     let tarea = new Object();
-    
+
     tarea.idTarea = idTarea;
     tarea.tarea = pTarea;
     tarea.prioridad = pPrioridad;
-    
+
 
 
 
@@ -95,16 +99,84 @@ function guardarTarea(pTarea, pPrioridad) {
 
 //filtrado
 
+//filtrar por nombre
+//creo un nuevo array para almacenar la lista filtrada por nombre de tarea
+
+var listaFiltrada = new Array();
+
+var buscar = document.querySelector('#buscarTarea');
+
+buscar.addEventListener('keydown', (event) => {
+    let buscarTarea = event.target.value
+    //console.log(buscarTarea) se guardan las letras.
+
+    listaFiltrada = listaTareas.filter(function (tarea) {
+        let tituloTarea = tarea.tarea.toLowerCase()
+
+        return tituloTarea.includes(buscarTarea);
+    })
+
+    pintarTareasFiltradas(listaFiltrada); //creo una funcion que llamo mas adelante para pintar el nuevo array
+})
+
+//filtrar prioridad
+//creo un nuevo array para almacenar la lista filtrada por prioridad
+var listaPrioridad = new Array();
+
+var prioridad = document.querySelector('#buscarPrioridad');
+
+prioridad.addEventListener('change', function (event) {
+    let filtroPrioridad = event.target.value;
+    console.log(filtroPrioridad) //se muestra la prioridad.
+
+    listaPrioridad = listaTareas.filter(function (tarea) {
+        tarea.prioridad.includes(filtroPrioridad)
+    })
+
+    pintarTareasFiltradas(listaPrioridad); //creo una funcion para pintar el nuevo array
+})
 
 
+//esta funcion pinta las tareas YA FILTRADAS.
 
+function pintarTareasFiltradas(pListaFiltro) {
+
+    seccionTareas.innerHTML = "";
+
+    if (pListaFiltro.length != ""){
+        //si el imput NO está vacio, pinte las tareas que pasan el filtro
+
+        pListaFiltro.forEach(pObjetoTarea => {
+            seccionTareas.innerHTML += `<article class="${pObjetoTarea.prioridad}">
+                                            <div id="${pObjetoTarea.idTarea}" class="eliminar">Eliminar</div>
+                                            <h3>${pObjetoTarea.tarea}</h3>
+                                        </article>`
+        })
+
+    } else {
+        //le digo que SI el imput queda vacio, deje todas las tareas.
+        pintarUnaTarea(tarea);
+    }
+
+    //y añado la funcion de eliminar para que pueda eliminar las tareas aunque esten filtradas. FUNCIONA.
+    var listaBotones = document.querySelectorAll('.eliminar');
+
+    for (eliminar of listaBotones) {
+        eliminar.addEventListener('click', botonEliminar);
+    }
+
+}
 
 //boton eliminar
 
-var eliminar = document.querySelector('.eliminar');
 
-eliminar.on('click', botonEliminar);
+function botonEliminar(event) {
+    //console.log(event.target.id);
 
-function botonEliminar(event){
-    seccionTareas.removeChild(pIdTarea)
+    //borro del array con un splice, metiendole el id como primer parametro. Solo borra las existentes, no las que creo.
+    listaTareas.splice(event.target.id, 1);
+
+    let articleBorrar = event.target.parentNode;
+    //console.log(articleBorrar)
+    seccionTareas.removeChild(articleBorrar);
 }
